9b25969b4b6535e56e199908087a5998a5156c51
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2022-10-15 18:56:37 +0100

    stable. waiting on favicon

---
+++ src/lib/Section.svelte
@@ -0,0 +1,10 @@
+<div>
+    <slot></slot>
+</div>
+
+<style>
+    div {
+        height: 100%;
+        /* background-color: azure; */
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
-    import '../app.css'
+    import "../app.css";
 </script>
 
-<slot/>
+<div class="bg-img" />
+<slot />
+
+<style>
+    .bg-img {
+        background: url("/bg-img-portrait.svg");
+        position: fixed;
+        width: 100vw;
+        min-height: 100vh;
+        background-size: cover;
+        z-index: -10;
+    }
+
+    @media screen and (min-width: 820px) {
+        .bg-img {
+            background: url("/bg-img.svg");
+        }
+    }
+</style>
